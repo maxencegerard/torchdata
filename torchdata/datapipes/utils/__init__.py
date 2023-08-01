19d724a53d164e994c060c00b6ce590addfcc768
maxence.gerard <maxence.gerard@storyzy.com>
2023-08-01 14:41:51 +0200

    Fix circular import bug related to StreamWrapper

--- torchdata/datapipes/utils/__init__.py
+++ torchdata/datapipes/utils/__init__.py
@@ -4,11 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from torchdata.datapipes.utils.stream_wrapper import StreamWrapper
 
 from torchdata.datapipes.utils._visualization import to_graph
 from torchdata.datapipes.utils.janitor import janitor
 from torchdata.datapipes.utils.pin_memory import pin_memory_fn
-from torchdata.datapipes.utils.stream_wrapper import StreamWrapper
 
 
 __all__ = [
